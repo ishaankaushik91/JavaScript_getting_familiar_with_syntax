6c39dfcbf52567a72e55c8d33bc076a50fecfdee
ishaankaushik91 <ishaankaushik91@gmail.com>
2022-01-19 09:29:47 +0530

    Some Code

---
+++ 30.js
@@ -0,0 +1,14 @@
+// Printing all elements of objects one by one (LOOP)
+let Humans = {
+
+    1 : "Hello Uncle Namaste",
+    2 : 89,
+    3 : 13,
+    4 : 12,
+    5 : 11
+}
+
+for (let i = 1; i <= 5; i++)
+{
+    console.log(Humans[i]);
+}
